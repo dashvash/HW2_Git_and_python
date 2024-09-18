5f19f4f8453da9205565f4c39b0ab5dc0e6084f1
vickulenich <rusfan.ares@gmail.com>
2024-09-19 00:24:31 +0700

    Add divide function

--- calculator.py
+++ calculator.py
@@ -1,10 +1,15 @@
-def fun_minus(a,c):
-   return float(a) - float(c)
+def fun_minus(num_1,num_2):
+   return float(num_1) - float(num_2)
+
+def fun_div(num_1,num_2):
+   return float(num_1) / float(num_2)
 
 def main():
     line = str(input()).split()
     num_1 = line[0]
     num_2 = line[2]
     operator = line[1]
-    if operator == "-": 
+    if operator == "-":
       return fun_minus(num_1,num_2)
+    if operator == "/":
+      return fun_div(num_1,num_2)
