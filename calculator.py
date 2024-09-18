780bdd8fefeeeb23d9cf02388a38d955985462e9
vickulenich <rusfan.ares@gmail.com>
2024-09-18 23:51:23 +0700

    Added divide function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
+def fun_div(a, c):
+	return float(a) / float(c)
+
 def main():
-    line = str(input()).split()
-    num_1 = line[0]
-    num_2 = line[2]
-    operator = line[1]
+	line = str(input()).split()
+	num_1 = line[0]
+	num_2 = line[2]
+	operator = line[1]
+
+	if operator == "/":
+    		return fun_div(num_1,num_2)
